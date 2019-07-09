da134fae82b68c06f993a54281e7e91b64ba35d7
zhk <yxzhk@hotmail.com>
2019-07-09 21:01:47 +0800

    a few source code change

--- underGround/contracts/Token.sol
+++ underGround/contracts/Token.sol
@@ -1,15 +1,15 @@
 pragma solidity ^0.4.24;
 
 contract Token {
-    mapping (address=>uint256) public balanceOf;
+    mapping (address => uint256) public balanceOf;
 
     constructor (uint256 initialSupply) public {
         balanceOf[msg.sender] = initialSupply;
     }
 
     function transfer(address _to, uint256 _value) public {
-        require(balanceOf[msg.sender]>=_value);
-        require(balanceOf[_to] + _value >=balanceOf[_to]);
+        require(balanceOf[msg.sender] >= _value);
+        require(balanceOf[_to] + _value >= balanceOf[_to]);
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
     }
